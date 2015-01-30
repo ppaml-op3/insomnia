a385105e0edc2a81ee970c85f635eadea996d795
Aleksey Kliger <aleksey@lambdageek.org>
2015-01-30 07:58:11 -0500

    [minor] AST for F-Omega

---
+++ src/FOmega/Syntax.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, MultiParamTypeClasses #-}
+module FOmega.Syntax where
+
+import Data.Typeable (Typeable)
+import GHC.Generics (Generic)
+
+import Unbound.Generics.LocallyNameless
+
+-- | There will be some stylized records that have predefined field
+-- names distinct from what a user may write.
+data Field =
+  FVal
+  | FType
+  | FSig
+  | FUser !String
+    deriving (Show, Eq, Typeable, Generic)
+
+data Kind =
+  KType
+  | KArr !Kind !Kind
+    deriving (Show, Eq, Typeable, Generic)
+
+type TyVar = Name Type
+
+data Type =
+  TV !TyVar
+  | TLam !(Bind (TyVar, Embed Kind) Type)
+  | TApp !Type !Type
+  | TForall !(Bind (TyVar, Embed Kind) Type)
+  | TExist !ExistPack
+  | TRecord ![(Field, Type)]
+  | TArr !Type !Type
+  deriving (Show, Typeable, Generic)
+
+type ExistPack = Bind (TyVar, Embed Kind) Type
+
+type Var = Name Term
+
+data Term =
+  V !Var
+  | Lam !(Bind (Var, Embed Type) Term)
+  | App !Term !Term
+  | PLam !(Bind (TyVar, Embed Kind) Term)
+  | PApp !Term !Type
+  | Record ![(Field, Term)]
+  | Proj !Term !Field
+  | Pack !Term !Type !ExistPack
+  | Unpack !(Bind (TyVar, Var, Embed Term) Term)
+  deriving (Show, Typeable, Generic)
+
+-- * Alpha equivalence and Substitution
+
+
+instance Alpha Field
+instance Alpha Kind
+instance Alpha Type
+instance Alpha Term
+
+instance Subst Type Type where
+  isvar (TV a) = Just (SubstName a)
+  isvar _ = Nothing
+
+-- no types inside kinds
+instance Subst Type Kind where
+  subst _ _ = id
+  substs _ = id
+instance Subst Type Field where
+  subst _ _ = id
+  substs _ = id
+
+instance Subst Term Term where
+  isvar (V a) = Just (SubstName a)
+  isvar _ = Nothing
+
+instance Subst Term Type where
+  subst _ _ = id
+  substs _ = id
+
+instance Subst Term Field where
+  subst _ _ = id
+  substs _ = id
+
+instance Subst Term Kind where
+  subst _ _ = id
+  substs _ = id
